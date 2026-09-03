cdca0921c3dc6ab52a8defdf2a11ff99ed4d42f8
Cowanbas <kauanfonsecaruizcarvalho@gmail.com>
2026-09-03 15:39:31 -0300

    Desisto de repartir o código

--- app/src/main/java/ui/theme/CompositionLocals.kt
+++
@@ -1,6 +0,0 @@
-package com.cowanbas.gym.ui.theme
-
-import androidx.compose.runtime.compositionLocalOf
-
-val LocalAdvancedMode = compositionLocalOf { false }
-val LocalWeightUnit = compositionLocalOf { "kg" }
